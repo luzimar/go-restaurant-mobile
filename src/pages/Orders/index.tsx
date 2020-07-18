740c89144affddfd55171194f13ca9e00d6c822f
Luzimar Oliveira <lzm.spfc@gmail.com>
2020-07-18 14:13:59 -0300

    Add funcionalidades

---
+++ src/pages/Orders/index.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import { Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import api from '../../services/api';
+import formatValue from '../../utils/formatValue';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  FoodsContainer,
+  FoodList,
+  Food,
+  FoodImageContainer,
+  FoodContent,
+  FoodTitle,
+  FoodDescription,
+  FoodPricing,
+} from './styles';
+
+interface Food {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  formattedValue: number;
+  thumbnail_url: string;
+}
+
+const Orders: React.FC = () => {
+  const [orders, setOrders] = useState<Food[]>([]);
+  const navigation = useNavigation();
+  useEffect(() => {
+    async function loadOrders(): Promise<void> {
+      const { data } = await api.get<Food[]>('/orders');
+      const response = data.map(order => ({
+        ...order,
+        formattedValue: formatValue(order.price),
+      }));
+      setOrders(response);
+    }
+
+    loadOrders();
+  }, []);
+  async function handleNavigate(id: number): Promise<void> {
+    navigation.navigate('FoodDetails', {
+      id,
+    });
+  }
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>Meus pedidos</HeaderTitle>
+      </Header>
+
+      <FoodsContainer>
+        <FoodList
+          data={orders}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Food
+              key={item.id}
+              activeOpacity={0.6}
+              onPress={() => handleNavigate(item.id)}
+            >
+              <FoodImageContainer>
+                <Image
+                  style={{ width: 88, height: 88 }}
+                  source={{ uri: item.thumbnail_url }}
+                />
+              </FoodImageContainer>
+              <FoodContent>
+                <FoodTitle>{item.name}</FoodTitle>
+                <FoodDescription>{item.description}</FoodDescription>
+                <FoodPricing>{item.formattedValue}</FoodPricing>
+              </FoodContent>
+            </Food>
+          )}
+        />
+      </FoodsContainer>
+    </Container>
+  );
+};
+
+export default Orders;
